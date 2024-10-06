f96f2f73a49466569e69185dbc49705b5820a0bc
jarowl <jaroslav.hodonicky@email.cz>
2024-10-06 11:44:37 +0000

    v1.0

--- first.py
+++ first.py
@@ -1,9 +1,9 @@
 # funkce vypise "Číslo X je sudé" pokud je cislo sude a "Číslo X je liché" pokud je cislo liche
 def sudy_nebo_lichy(cislo):
     if cislo%2 == 0:
-        print("Cislo", cislo, "je sude")
+        print(f"Cislo {cislo} je sude")
     else:
-        print("Cislo", cislo, "je liche")
+        print(f"Cislo {cislo} je liche")
 #test
 sudy_nebo_lichy(5)
 sudy_nebo_lichy(1000000)
