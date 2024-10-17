9c8748c02a572003a1b057e7843e9a9398bc148a
jarowl <jaroslav.hodonicky@email.cz>
2024-10-17 13:53:14 +0000

    final_ver

--- second.py
+++ second.py
@@ -1,18 +1,18 @@
-def userInputLimit():
+def userInputLimit(cislo):
     #\/\/\ user limitations \/\/\
-    num_input = input("Zadej číslo od 0 do 100: ")
+    num_input = cislo
     num = int(num_input)
     if (num) < 0 or (num) > 100:
-        while (num) < 0 or (num) > 100:
-            num_input = input("Špatně zadané číslo. Zadej znovu číslo od 0 do 100: ")
-        return (num)
+        return None
     else:
         return (num)
 
 def cislo_text (cislo):
     # funkce, která zkonvertuje číslo do náležité textové reprezentace.
-    num = int(cislo)
-    numDict = {
+    num = userInputLimit(cislo)
+    if num != None :
+        num = int(cislo)
+        numDict = {
         0: "nula",
         1: "jedna",
         2: "dva",
@@ -34,31 +34,32 @@ def cislo_text (cislo):
         90: "devadesát",
         100: "sto"
     }
-    match num:
-        case 11:
-            return "jedenáct"
-        case 14:
-            return "čtrnáct"
-        case 15:
-            return "patnáct"
-        case 19:
-            return "devatenáct"
-        case 100:
-            return numDict[num]
+        match num:
+            case 11:
+                return "jedenáct"
+            case 14:
+                return "čtrnáct"
+            case 15:
+                return "patnáct"
+            case 19:
+                return "devatenáct"
+            case 100:
+                return numDict[num]
 
-    if num > 11 and num < 20:
-        lt = str(num/10).split(".",2)
-        jednotky = int(lt[1])
-        return numDict[jednotky] + "náct"
-    elif (num) > 20 and num not in numDict:
-        lt = str(num/10).split(".",2)
-        desitky = int(lt[0]) * 10
-        jednotky = lt[1]
-        return numDict[desitky] + " " + numDict[int(jednotky)]
-    else:
-        return numDict[int(num)]
+        if num > 11 and num < 20:
+            lt = str(num/10).split(".",2)
+            jednotky = int(lt[1])
+            return numDict[jednotky] + "náct"
+        elif (num) > 20 and num not in numDict:
+            lt = str(num/10).split(".",2)
+            desitky = int(lt[0]) * 10
+            jednotky = lt[1]
+            return numDict[desitky] + " " + numDict[int(jednotky)]
+        else:
+            return numDict[int(num)]
+    
+    return None
     
 if __name__ == "__main__":
-    cislo = userInputLimit()
-    text = cislo_text(cislo)
+    text = cislo_text("112")
     print(text)
