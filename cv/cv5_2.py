3d18cc7653901da2f86f6183fb868af1eaa06e4b
jarowl <jaroslav.hodonicky@email.cz>
2024-10-31 13:30:44 +0000

    cv5

--- cv/cv5_2.py
+++ cv/cv5_2.py
@@ -1,15 +1,30 @@
 import sys
+import csv
 
-def read_data(file_name):
-    data = []
-    # TODO
-    return data
+def read_data(soubor):
+      data = []
+      with open(soubor, "r") as file:         #otevre soubor, r mode a priradi to file
+        reader = csv.reader(file)           #reader - csv.funkce z knihovny csv
+        for radek in reader:                #
+            data.append(radek)
+        return data
+
+def read_data_split(soubor):
+      data = []
+      with open(soubor, "r") as file:         #otevre soubor, r mode a priradi to file
+        for line in file:
+            data.append(line.strip())
+        return data
 
 if __name__ == "__main__":
     try:
         file = sys.argv[1]
         data = read_data(file)
         print(data)
+
+        data = read_data_split(file)
+        print(data)
+
     except IndexError:
         print("Nebyly zadány soubory.")
     except FileNotFoundError:
