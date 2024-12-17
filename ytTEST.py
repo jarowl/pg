932e2f71230189cbb4eb8debec172734a943620d
jarowl <jaroslav.hodonicky@email.cz>
2024-12-17 16:25:55 +0000

    .

--- ytTEST.py
+++
@@ -1,4 +0,0 @@
-from ytTEST import YouTube
-yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-
-yt.title
---
+++ ytb.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://youtu.be/eICijnr1k_I'])
+
+    ydl.u
