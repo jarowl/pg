770f3fab19c667ab1527cd13043c97d7432f4340
jarowl <jaroslav.hodonicky@email.cz>
2024-12-18 14:04:09 +0000

    eighth

--- eighth.py
+++ eighth.py
@@ -1,16 +1,24 @@
 def bin_to_dec(binarni_cislo):
-    # funkce spocita hodnotu predavaneho binarniho cisla (binarni_cislo muze byt str i int!!!)
-    # 111 -> 7
-    # "101" -> 5
-    
+    # Pokud je binární číslo typu int, převedeme ho na řetězec
+    if isinstance(binarni_cislo, int):
+        binarni_cislo = str(binarni_cislo)
+    # Funkce int() s parametrem 2 přetvoří binární řetězec na desítkový
+    return int(binarni_cislo, 2)
 
-    pass
-
-
-def test_funkce():
+def test_bin_to_dec():
+    # Testy pro funkci bin_to_dec()
     assert bin_to_dec("0") == 0
     assert bin_to_dec(1) == 1
     assert bin_to_dec("100") == 4
     assert bin_to_dec(101) == 5
     assert bin_to_dec("010101") == 21
     assert bin_to_dec(10000000) == 128
+    assert bin_to_dec("10011101") == 157  # Opravené číslo na správný výsledek pro 10011101
+    print("Všechny testy proběhly úspěšně!")
+
+# Volání testu
+test_bin_to_dec()
+
+if __name__ == "__main__":
+
+    print(bin_to_dec(10011101))
