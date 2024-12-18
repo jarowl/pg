9e10946476df26d076462fe92b425b40a83686d8
jarowl <jaroslav.hodonicky@email.cz>
2024-12-18 12:59:04 +0000

    y

--- ytb.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-import youtube_dl
-
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://youtu.be/eICijnr1k_I'])
-
-    ydl.u
